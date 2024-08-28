274de2b2ea0e641ff44f51428f4a33ec35a99e55
Douglas Antonni Alves De Lima <127199046+DouglasAntonni@users.noreply.github.com>
2024-08-28 12:19:28 -0300

    inicializando projeto

---
+++ Program.cs
@@ -0,0 +1,57 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi(); // Sem ponto e vírgula após este método
+
+app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login Realizado com sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class LoginDTO
+{
+    public string Email { get; set; } = string.Empty;
+    public string Senha { get; set; } = string.Empty;
+}
